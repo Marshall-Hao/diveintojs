69863cf6f4713577f77d85bea2979c6b868f6989
Marshallccc <Marshallchan666@outlook.com>
2021-09-05 02:20:01 +0800

    windows

---
+++ basic35window/script.js
@@ -0,0 +1,7 @@
+console.log(window);
+
+window.console.log("hi"); // !console is a property of window, normally we dont write window out
+// ! if type sth without window word, always recoganize with a window word
+const alert = "msg";
+
+window.alert("oops"); //! if the method accidently named as a variable, then need to put window ahead
