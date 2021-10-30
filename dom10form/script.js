c2d711a91f82da42616f9245a2031dda5a193725
Marshallccc <Marshallchan666@outlook.com>
2021-10-30 23:54:57 +0800

    form feature

---
+++ dom10form/script.js
@@ -0,0 +1,23 @@
+//* form 方法
+
+// * validation
+// * required for not empty
+
+// * pattern for regex
+
+// * maxlength minlength
+
+// * Form validation Api
+
+/*
+
+*/
+
+// * form submit
+
+/*
+ * action:  url
+ * method : get || post
+ * enctype:
+ * target: _self, _blank, _parent, _top
+ */
